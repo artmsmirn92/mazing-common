844fbc0ae0d717f58b2d7db51be4d67865df2939
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2023-01-16 12:24:05 +0300

    possibility to get current font through ILocalizationManager without language

--- Runtime/Managers/ILocalizationManager.cs
+++ Runtime/Managers/ILocalizationManager.cs
@@ -1,10 +1,11 @@
 ﻿using mazing.common.Runtime.Entities;
 using mazing.common.Runtime.Enums;
+using TMPro;
 using UnityEngine.Events;
 
 namespace mazing.common.Runtime.Managers
 {
-    public interface ILocalizationManager : IInit, IFontProvider
+    public interface ILocalizationManager : IInit
     {
         event UnityAction<ELanguage> LanguageChanged;
         string                       GetTranslation(string _Key);
@@ -12,5 +13,7 @@ namespace mazing.common.Runtime.Managers
         ELanguage                    GetCurrentLanguage();
         void                         AddTextObject(LocalizableTextObjectInfo    _Info);
         void                         RemoveTextObject(LocalizableTextObjectInfo _Info);
+
+        TMP_FontAsset GetFont(ETextType _TextType, ELanguage? _Language = null);
     }
 }
