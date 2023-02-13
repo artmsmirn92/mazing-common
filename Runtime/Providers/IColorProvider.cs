118a9403b2332f2ede2b6f66478be1a5e330b5c3
Artem Smirnov <56093325+semi92art@users.noreply.github.com>
2023-02-13 07:09:07 +0300

    add UpdateColor() method to IColorProvider

--- Runtime/Providers/IColorProvider.cs
+++ Runtime/Providers/IColorProvider.cs
@@ -6,9 +6,12 @@ namespace mazing.common.Runtime.Providers
     public interface IColorProvider : IInit
     {
         event UnityAction<int, Color>  ColorChanged;
-        void                           AddIgnorableForThemeSwitchColor(int    _ColorId);
-        void                           RemoveIgnorableForThemeSwitchColor(int _ColorId);
-        Color                          GetColor(int                           _Id);
-        void                           SetColor(int                           _Id, Color _Color);
+
+        void AddIgnorableForThemeSwitchColor(int    _ColorId);
+        void RemoveIgnorableForThemeSwitchColor(int _ColorId);
+
+        Color GetColor(int    _Id);
+        void  SetColor(int    _Id, Color _Color);
+        void  UpdateColor(int _Id);
     }
 }
